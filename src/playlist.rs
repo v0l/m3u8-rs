65be582b758ba04213701cddf201febe6fab49ce
Kieran <kieran@harkin.me>
2025-06-16 16:06:57 +0100

    fix: write unknown tags before segments

--- src/playlist.rs
+++ src/playlist.rs
@@ -809,15 +809,16 @@ impl MediaPlaylist {
         if let Some(ref start) = self.start {
             start.write_to(w)?;
         }
+
+        for unknown_tag in &self.unknown_tags {
+            writeln!(w, "{}", unknown_tag)?;
+        }
+
         for segment in &self.segments {
             segment.write_to(w)?;
         }
         if self.end_list {
             writeln!(w, "#EXT-X-ENDLIST")?;
-        }
-
-        for unknown_tag in &self.unknown_tags {
-            writeln!(w, "{}", unknown_tag)?;
         }
 
         Ok(())
